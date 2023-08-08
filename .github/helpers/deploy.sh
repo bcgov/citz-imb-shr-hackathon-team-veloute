2322d5e47fdc6557287e0e57e9ab473da0b30c35
Adam Kroon <Adam.Kroon@gov.bc.ca>
2023-08-08 12:01:56 -0700

    Create deploy.sh

---
+++ .github/helpers/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# script to process either the DPIA app or api deployment config templates and deploy it to a given namespace
+
+oc process -f /home/runner/work/citz-imb-shr-hackathon-team-veloute/citz-imb-shr-hackathon-team-veloute/openshift/templates/api/$DC_TEMPLATE --namespace=$NAMESPACE \
+    -p APPLICATION_NAME=$APPLICATION_NAME \
+    -p LICENSE_PLATE=$LICENSE_PLATE \
+    -p IMAGESTREAM=$IMAGESTREAM \
+    -p ENVIRONMENT=$ENVIRONMENT | \
+    oc apply -f -
