2065f7f0595ef243b96347110da0551f67bf3767
dbarkowsky <dylanbarkowsky@gmail.com>
2023-08-16 11:23:50 -0700

    Added env and using env in API

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,9 @@
 import app from "./express";
+import Constants from "./constants";
 
-app.listen(6000, (err?: Error) => {
+const { API_PORT } = Constants;
+
+app.listen(API_PORT, (err?: Error) => {
   if (err) console.log(err);
-  console.info(`Server started on port 6000.`);
+  console.info(`Server started on port ${API_PORT}.`);
 });
