3f8b22b737e7c291bc004f9f24a13bcee8821304
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-08-17 14:40:59 -0700

    WIP

--- src/frontend/src/views/HM/HM.tsx
+++ src/frontend/src/views/HM/HM.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SearchBar, CustomSelect } from '../../components';
 import { useState } from 'react';
+import axios from 'axios';
+import constants from '../../constants/Constants';
+import { TextField, Button } from '@mui/material';
+import Box from '@mui/material/Box';
 
 import {
   ViewContainer,
@@ -11,13 +17,15 @@ export default function HM() {
   const [positionNumber, setPositionNumber] = useState('');
   const [experience, setExperience] = useState('');
   const [positionData, setPositionData] = useState([{}])
-  const [positionExists, setPositionExists] = useState(false);
+  const [positionExists, setPositionExists] = useState(true);
   const [jobTitle, setJobTitle] = useState('');
   const [ministry, setMinistry] = useState('');
   const [band, setBand] = useState('');
   const [titleOptions, setTitleOptions] = useState([{ value: '', label: ''}]);
   const [ministryOptions, setMinistryOptions] = useState([{ value: '', label: ''}]);
   const [bandOptions, setBandOptions] = useState([{ value: '', label: ''}]);
+
+  const [inputValue, setInputValue] = useState('');
 
   const ExperienceLevels = [
     { value: '1', label: '1'},
@@ -26,15 +34,41 @@ export default function HM() {
     { value: '4', label: '4'},
   ];
 
-  // TODO: GET to position number to get positionData
-  const handleSearchSubmit = (searchTerm: string) => {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const data = {
+      "positionNum": {positionNumber},
+      "Experience": {experience},
+      "Title": {jobTitle},
+      "Ministry": {ministry},
+      "Band": {band},
+      "Comments": {inputValue},
+    };
+    axios.post(`${constants.BACKEND_URL}/api/submission`, data)
+    .catch((err) => {
+      console.log(err);
+    });
+    setPositionNumber('');
+  };
+
+  const handleChange = (event: any) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearchSubmit = async (searchTerm: string) => {
     setPositionNumber(searchTerm);
     if (searchTerm.length < 8) {
       const zerosToAdd = Math.max(0, 8 - searchTerm.length);
       const leadingZeros = '0'.repeat(zerosToAdd);
       setPositionNumber(leadingZeros + searchTerm);
     }
-    console.log('Search term:', positionNumber);
+    const response = await axios.get(`${constants.BACKEND_URL}/api/searchPosition`);
+    setPositionData(response.data);
+    setPositionExists(true);
+    setJobTitle(response.data.title);
+    setMinistry(response.data.ministry);
+    setBand(response.data.band.name);
+    console.log('Search term:', response);
   };
 
   const handleExperienceSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -110,6 +144,20 @@ export default function HM() {
             />
           </>
         )}
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ p: '10px 0px 0px 0px' }} >
+            <TextField
+              label="Enter text"
+              variant="outlined"
+              value={inputValue}
+              onChange={handleChange}
+              fullWidth
+            />
+          </Box>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+      </form>
       </ContentContainer>
     </ViewContainer>
   )
